ade37030a4532755fdbfa579c0ed8b3bb4572188
David Ainslie <dainslie@gmail.com>
2014-10-24 18:31:29 +0100

    WIP

--- app/filters/JsonFilter.scala
+++ app/filters/JsonFilter.scala
@@ -3,15 +3,17 @@ package filters
 import play.api.Logger
 import play.api.mvc.{EssentialFilter, EssentialAction, RequestHeader}
 
-object JsonFilter extends EssentialFilter {
+// TODO Working on
+/*object JsonFilter extends EssentialFilter {
   def apply(nextFilter: EssentialAction) = new EssentialAction {
     def apply(requestHeader: RequestHeader) = nextFilter(requestHeader).map { result =>
       result.
       result
     }
   }
-}
+}*/
 
+/*
 object LoggingFilter extends EssentialFilter {
   def apply(nextFilter: EssentialAction) = new EssentialAction {
     def apply(requestHeader: RequestHeader) = {
@@ -25,4 +27,4 @@ object LoggingFilter extends EssentialFilter {
       }
     }
   }
-}
+}*/

40f27aec1ae074fbf8dd5e4a0bcc5228be0b04be
David Ainslie <dainslie@gmail.com>
2014-11-30 15:17:33 +0000

    Heroku Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: target/universal/stage/bin/registered-traveller-prototype -Dhttp.port=$PORT -Dconfig.resource=conf/application.production.conf
--- app/Global.scala
+++ app/Global.scala
@@ -1,7 +1,17 @@
-import play.api.GlobalSettings
+import java.io.File
+
+import play.api.Mode.Mode
+import play.api.{Configuration, GlobalSettings}
 import play.api.mvc.WithFilters
 import play.filters.headers.SecurityHeadersFilter
 
 /*
 object Global extends WithFilters(SecurityHeadersFilter()) with GlobalSettings {
 }*/
+
+object Global extends GlobalSettings {
+  override def onLoadConfig(config: Configuration, path: File, classloader: ClassLoader, mode: Mode) = {
+    println(s"""===> Owner = ${config.getString("owner")}""")
+    super.onLoadConfig(config, path, classloader, mode)
+  }
+}
